620ff16c3cebd92b1247fd5f37584a383081eb8e
津村寛達 <TSUMUHIRO@MacBook-Air.local>
2021-05-05 23:13:30 +0900

    2021.0505.test

---
+++ hangman.py
@@ -0,0 +1,40 @@
+
+
+def start(word) :
+    wrong = 0
+    stages = ["",
+                "__________    ",
+                "|         |   ",
+                "|         O   ",
+                "|        /|\  ",
+                "|         |   ",
+                "|        / \  ",
+                "|             "
+                ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+
+    print("ハングマンにようこそ！！")
+
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "1文字を予想してね！"
+        char = input(msg)
+        if char in rletters :
+            chind = rletters.index(char)
+            board[chind] = char
+            rletters[chind] = '$'
+        else :
+            wrong += 1
+        print(" ".join(board))
+        e = wrong + 1
+        print("\n".join(stages[0:e])) 
+        if "_" not in board :
+            print("あなたの勝ち！")
+            print(" ".join(board))
+            win = True
+            break
+    if not win :
+        print("\n".join(stages[0:wrong+1]))
+        print("あなたの負け！　正解は{}.".format(word))
